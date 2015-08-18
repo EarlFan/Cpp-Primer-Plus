1f230fc44859e24ad3e7e30544d2b3100d3712f8
Z-Gu <z_gu@outlook.com>
2015-08-18 23:01:42 +0100

    Chapter 2

---
+++ 2/2_1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "Zhengyang Gu\nEmmanuel College\nCambridge\nCB2 3AP\nUnited Kingdom\n" ;
+	return 0;
+}
---
+++ 2/2_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "Please enter the distance in furlongs\n";
+	double n = 0;
+	cin >> n;
+	cout << "The distance in yards is : " << n * 220 << endl;
+	return 0;
+}
